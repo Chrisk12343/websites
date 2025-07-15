f51ff97c34388e354ac2ee6cae51dcfc92ddbf87
Chrisk12343 <CKRAWCZYK89964@uat.edu>
2025-07-15 05:56:20 -0500

    Add files via upload

---
+++ demojava.js
@@ -0,0 +1,6 @@
+
+ document.getElementById("Name").textContent= "Christopher Krawczyk",
+    document.getElementById("Major").textContent= "Computer Science"
+    document.getElementById("Adress").textContent= "Ckrawczyk89964@uat.edu"
+    document.getElementById("GDate").textContent= "May 2025"
+ 
