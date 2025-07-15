744521c28f3a825629615376a52d5499a867d68d
Chrisk12343 <CKRAWCZYK89964@uat.edu>
2025-07-15 06:12:57 -0500

    Add files via upload

---
+++ demojava.js
@@ -0,0 +1,7 @@
+
+ document.getElementById("Name").textContent= "Christopher Krawczyk",
+    document.getElementById("Major").textContent= "Computer Science"
+    document.getElementById("Adress").textContent= "Ckrawczyk89964@uat.edu"
+    document.getElementById("GDate").textContent= "May 2025"
+    // i take my id and put it here then put what i want to say at the end and it shows up on my webpage.
+ 
