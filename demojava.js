06f6b48a7499f70da305fa68d943089b6c4565bb
Chrisk12343 <CKRAWCZYK89964@uat.edu>
2025-07-15 06:12:39 -0500

    Delete demojava.js

--- demojava.js
+++
@@ -1,6 +0,0 @@
-
- document.getElementById("Name").textContent= "Christopher Krawczyk",
-    document.getElementById("Major").textContent= "Computer Science"
-    document.getElementById("Adress").textContent= "Ckrawczyk89964@uat.edu"
-    document.getElementById("GDate").textContent= "May 2025"
- 
